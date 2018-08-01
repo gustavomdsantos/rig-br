a02a904670a8feaa472b8e0fedfe722b3a3585ae
Gustavo Moraes <gustavosotnas1@gmail.com>
2018-08-01 11:25:31 -0300

    Corrige erro na execução da função 'compile()' no Windows

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -5,12 +5,16 @@
 # Autor: Gustavo Moraes <gustavosotnas1@gmail.com>
 # MIT License – Copyright (c) 2018 Gustavo Moraes
 
-import json, sys, shutil, subprocess
+import sys, os, json, shutil, subprocess
 
 defaultPaths = {
-	'dist': '../dist',
+	'build': 'build',
+	'dist': 'dist',
+	'spec': 'rig-br.spec',
 	'src': '../src/rig-br.py',
-	'icon': '../../rig-br.wiki/icon/rig-br.ico'
+	'icon': '../../rig-br.wiki/icon/rig-br.ico',
+	'iscc': 'C:\Program Files (x86)\Inno Setup 5\ISCC.exe',
+	'iss': 'innosetup/rig-br.iss'
 }
 
 def readManifestFile():
@@ -19,7 +23,7 @@ def readManifestFile():
 	print(data)
 
 # Source:
-# https://www.webucator.com/how-to/how-check-the-operating-system-with-python.cfm
+#https://www.webucator.com/how-to/how-check-the-operating-system-with-python.cfm
 def getPlatform():
 	platforms = {
 		'linux1' : 'Linux',
@@ -34,21 +38,33 @@ def getPlatform():
 
 def detectOS():
 	osName = getPlatform()
+	return osName
 	if osName is "Windows":
-		print("I'm running on a Windows PC!")
+		print("OS: Windows")
 	elif osName is "Linux":
-		print("I'm running on a Linux PC!")
+		print("OS: Linux")
 	else:
-		print("I don't know where I'm being running.")
+		print("OS: Unknown ("+ osName +")")
 
-def rmDistFolder():
-	distPath = defaultPaths['dist']
-	try:
-		print("Deleting '"+ distPath +"' folder...")
-		shutil.rmtree(distPath)
-		print("Deleted '"+ distPath +"' folder has been previously deleted.")
-	except FileNotFoundError as fnfe:
-		print("'"+ distPath +"' folder has been previously deleted.")
+def rm_R(fileOrFolder):
+	if os.path.isfile(fileOrFolder):
+		try:
+			print("Deleting '"+ fileOrFolder +"' file...")
+			os.remove(fileOrFolder)
+		except OSError as ose:
+			print("'"+ fileOrFolder +"' file has been previously deleted.")
+	else:
+		try:
+			print("Deleting '"+ fileOrFolder +"' folder...")
+			shutil.rmtree(fileOrFolder)
+			print("The folder '"+ fileOrFolder +"' folder has been deleted successfully.")
+		except FileNotFoundError as fnfe:
+			print("'"+ fileOrFolder +"' folder has been previously deleted.")
+
+def cleanBuild():
+	rm_R(defaultPaths['build'])
+	rm_R(defaultPaths['dist'])
+	rm_R(defaultPaths['spec'])
 
 def isPyinstallerInstalled():
 	commandOutput = subprocess.run('pyinstaller --version',
@@ -59,12 +75,42 @@ def compile():
 	src = defaultPaths['src']
 	icon = defaultPaths['icon']
 	commandInput = 'pyinstaller \''+ src +'\' -w -i \''+ icon +'\''
+	if getPlatform() is "Windows":
+		# Workaround about PyInstaller (doesn't run in Python shells on Windows)
+		commandInput = 'PowerShell -Command "& {'+ commandInput +'}"'
 	print(commandInput)
 	commandOutput = subprocess.run(commandInput, shell=True)
 	print(commandOutput.returncode)
 
+# def isInnoSetupInstalled():
+# 	iscc = defaultPaths['iscc']
+# 	commandInput = 'PowerShell -Command "&(“'+ iscc +'”)"'
+# 	#commandInput = 'PowerShell -Command "&(“'+ iscc +'” /?)"'
+# 	print(commandInput)
+# 	commandOutput = subprocess.run(commandInput,
+# 		shell=True)
+# 		#shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+# 	return True if commandOutput.returncode is 0 else False
+
+# def build():
+# 	iscc = defaultPaths['iscc']
+# 	iss = defaultPaths['iss']
+# 	print("Build started!")
+# 	commandInput = 'cmd.exe /C "'+ iscc +'" "'+ iss + '"'
+# 	print(commandInput)
+# 	commandOutput = subprocess.run(commandInput,
+# 		shell=True)
+# 		#shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+# 	return True if commandOutput.returncode is 0 else False
+
 readManifestFile()
 detectOS()
-rmDistFolder()
+cleanBuild()
 if isPyinstallerInstalled():
 	compile()
+# if getPlatform() is "Windows":
+# 	if isInnoSetupInstalled():
+# 		build()
+# 	else:
+# 		print("Build NOT started.")
+# build()
